c9b9ea505052626fca1e52e50a9e2754afbbe8ba
Myles Simmons <99770090+MylesSimm@users.noreply.github.com>
2025-03-28 22:15:13 -0400

    Update Main.java
    addded a db manager and connected to database

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,7 +1,20 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World.");
+        Connection connection = null;
+        String url = "Jbdc:mariadb://localhost:3306/test";
+        String user = "root";
+        String password = "root";
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException) {
+            e.printStackTrace();
+        }
+        System.out.println("Successfully connected");
+        
         }
     }
